ceeecf71cfcfdc58e14e5cc7aeb453f96b0a7058
Brian Duong <brianminhduong@gmail.com>
2022-06-20 20:36:35 -0400

    Add media query to button

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -13,12 +13,15 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
-    border: 1px solid #ccc;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) =>
+      props.invalid ? "rgb(218, 168, 163)" : "transparent"};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
@@ -28,15 +31,6 @@ const FormControl = styled.div`
     outline: none;
     background: #fad0ec;
     border-color: #8b005d;
-  }
-
-  &.invalid input {
-    border-color: red;
-    background: rgb(218, 168, 163);
-  }
-
-  &.invalid label {
-    color: red;
   }
 `;
 
@@ -64,7 +58,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid && "invalid"}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
